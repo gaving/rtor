3ef9b64630fc05b68fb18973dcc55400df46c8a0
Gavin Gilmour <gavin.gilmour@gmail.com>
2009-09-20 10:23:11 +0100

    bit more crude error andling

--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -60,24 +60,29 @@ class Torrent
 
     def initialize(hash)
         @hash = hash
-        @base_path = server_call("d.get_base_path", hash)
-        @name = server_call("d.get_base_filename", hash)
-        @uploaded = server_call("d.get_up_total", hash)
-        @downloaded = server_call("d.get_bytes_done", hash)
-        @size = server_call("d.get_size_bytes", hash)
-        @state = server_call("d.get_state", hash)
-        @complete = server_call("d.get_complete", hash)
-        @down_rate = server_call("d.get_down_rate", hash)
-        @up_rate = server_call("d.get_up_rate", hash)
-        @remaining = @size - @downloaded
-        @percentage = sprintf("%.2f", (Float(@downloaded) / Float(@size) * 100))
-        @ratio = sprintf("%.2f", Float(@uploaded) / Float(@size))
-        @size = help.number_to_human_size(@size)
-        @uploaded = help.number_to_human_size(@uploaded)
-        @remaining = help.number_to_human_size(@remaining)
-        @downloaded = help.number_to_human_size(@downloaded)
-        @down_rate = help.number_to_human_size(@down_rate)
-        @up_rate = help.number_to_human_size(@up_rate)
+
+        begin
+            @base_path = server_call("d.get_base_path", hash)
+            @name = server_call("d.get_base_filename", hash)
+            @uploaded = server_call("d.get_up_total", hash)
+            @downloaded = server_call("d.get_bytes_done", hash)
+            @size = server_call("d.get_size_bytes", hash)
+            @state = server_call("d.get_state", hash)
+            @complete = server_call("d.get_complete", hash)
+            @down_rate = server_call("d.get_down_rate", hash)
+            @up_rate = server_call("d.get_up_rate", hash)
+            @remaining = @size - @downloaded
+            @percentage = sprintf("%.2f", (Float(@downloaded) / Float(@size) * 100))
+            @ratio = sprintf("%.2f", Float(@uploaded) / Float(@size))
+            @size = help.number_to_human_size(@size)
+            @uploaded = help.number_to_human_size(@uploaded)
+            @remaining = help.number_to_human_size(@remaining)
+            @downloaded = help.number_to_human_size(@downloaded)
+            @down_rate = help.number_to_human_size(@down_rate)
+            @up_rate = help.number_to_human_size(@up_rate)
+        rescue => e
+            print "Error: %s\r\n" % e
+        end
     end
 
     def to_param
